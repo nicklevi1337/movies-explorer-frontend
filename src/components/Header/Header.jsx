0b2ec7cf2c57728e1f6a4a2d61778ea6af530178
PrettyLolli <yunghunters2015@gmail.com>
2023-11-06 11:21:27 +0300

    код изменен

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,77 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-//import { useState } from "react";
-//import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import Navigation from "../Navigation/Navigation";
-//import AccountLink from "../AccountLink/AccountLink";
+import AccountLink from "../AccountLink/AccountLink";
 import FormLink from "../FormLink/FormLink";
 import Logo from "./../ui/Logo/Logo";
-import AuthMobile from "./../AuthMobile/AuthMobile"
-import Hamburger from "./../Hamburger/Hamburger"
 
-function Header( props ) {
-  const { loggedIn,
-    hamburger,
-    onHandleHamburger,
-} = props;
+function Header({ isLoggedIn }) {
+  const [menu, setMenu] = useState();
+  const { pathname } = useLocation();
+  function handleOpenMenu() {
+    setMenu(!menu);
+  }
+
+  const headerMenu =
+    // pathname === "/" ||
+    pathname === "/movies" ||
+    pathname === "/saved-movies" ||
+    pathname === "/profile";
+
   return (
-    <>
-    { loggedIn ? (
-      <AuthMobile
-          hamburger={hamburger}
-          onHandleHamburger={onHandleHamburger}
-      />
-    ) : null}
-    <header className='header'>
-      <Logo />
-      { loggedIn ?
-          (
-              <>
-                  <Hamburger
-                      onHandleHamburger={onHandleHamburger}
-                  />
-                  <Navigation />
-                  <Link to='/profile' title='Аккаунт' className='header__link header__link_type_account'>
-                      <p className='header__account'>Аккаунт</p>
-                      <span className='header__image'/>
-                  </Link>
-              </>
-          ) :
-          (
-              <div className='header__sign'>
-                <FormLink />
+    <header className="header">
+      <div className="header__container">
+        <Logo />
+        {isLoggedIn && headerMenu && (
+          <>
+            <div
+              className={
+                menu
+                  ? "header__menu-overlay header__menu-overlay_active"
+                  : "header__menu-overlay"
+              }
+            >
+              <div
+                className={
+                  menu
+                    ? "header__menu-wrapper header__menu-wrapper_active"
+                    : "header__menu-wrapper "
+                }
+              >
+                <div className="header__menu">
+                  <Navigation onClick={handleOpenMenu} />
+                  <AccountLink onClick={handleOpenMenu} />
+                </div>
               </div>
-          )
-      }
+            </div>
+
+            <button
+              className="header__burger"
+              type="button"
+              onClick={handleOpenMenu}
+            ></button>
+            <button
+              className={
+                menu
+                  ? "header__close-btn header__close-btn_active"
+                  : "header__close-btn"
+              }
+              type="button"
+              onClick={handleOpenMenu}
+            ></button>
+          </>
+        )}
+        {isLoggedIn && pathname === "/" && <FormLink />}
+      </div>
     </header>
-    </>
   );
 }
 
 export default Header;
-
 
 /*
 <header className={pathname === "/" ? "header header_black" : "header"}>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,42 @@
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
-//import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-function Navigation() {
+function Navigation({ onClick }) {
+  const { pathname } = useLocation();
 
   return (
-    <nav className='header__navigation'>
-        <NavLink to='/movies' title='Фильмы' className='header__link header__link_type_films' activeClassName='header__link_active'>Фильмы</NavLink>
-        <NavLink to='/saved-movies' title='Сохранённые фильмы' className='header__link header__link_type_saved-films' activeClassName='header__link_active'>Сохранённые фильмы</NavLink>
-    </nav>
+    <nav className="navigation">
+    <NavLink
+      to="/"
+      className={`navigation__link navigation__link_type_main ${
+        pathname === "/" ? "navigation__link_active" : ""
+      }`}
+      onClick={onClick}
+    >
+      Главная
+    </NavLink>
+    <NavLink
+      to="/movies"
+      className={`navigation__link ${
+        pathname === "/movies" ? "navigation__link_active" : ""
+      }`}
+      onClick={onClick}
+    >
+      Фильмы
+    </NavLink>
+    <NavLink
+      to="/saved-movies"
+      className={
+        pathname === "/saved-movies"
+          ? "navigation__link navigation__link_active"
+          : "navigation__link"
+      }
+      onClick={onClick}
+    >
+      Сохранённые фильмы
+    </NavLink>
+  </nav>
   );
 }
 
